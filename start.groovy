e603e08c29f98fa5877a4ffbb9cb5ea20ac16941
devops <raju30.devops@gmail.com>
2019-01-24 17:20:42 +0530

    First commit

---
+++ start.groovy
@@ -0,0 +1,5 @@
+@Grab("springboot:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
+@Grab("springboot:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
+@ComponentScan("springboot.sample.service")
+class Start {
+}
